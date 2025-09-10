adf8ceee89ab5580684e9f4eb76871cd0e21d366
Manolo <manolodr95@gmail.com>
2025-09-10 17:23:48 -0400

    update_local
    commit_new_Local

--- localizacion_dom-master/dgii_reports/models/__init__.py
+++ localizacion_dom-master/dgii_reports/models/__init__.py
@@ -1,8 +1,10 @@
-# Part of Domincana Premium.
-# See LICENSE file for full copyright and licensing details.
+# © 2024 ISJO TECHNOLOGY, SRL (Daniel Diaz <daniel.diaz@isjo-technology.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import res_partner
 from . import dgii_report
 from . import account_invoice
 from . import account_account
 from . import account_tax
+from . import account_move
+from . import ir17
---
+++ localizacion_dom-master_old/l10n_do_pos/models/__init__.py
@@ -0,0 +1,6 @@
+from . import pos_config
+from . import pos_order
+from . import res_config_settings
+from . import pos_session
+from . import pos_payment_method
+from . import pos_payment
